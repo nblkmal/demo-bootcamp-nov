554585c350496365904ecb10bb6ca9399d015668
bxlr_ <nabil@runcloud.io>
2025-12-09 21:32:13 +0800

    fix: change button color from bg-black to bg-blue-500

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1600,7 +1600,7 @@
                 <ul class="flex gap-3 text-sm leading-normal">
                     <li>
                         <a href="/login"
-                            class="inline-block dark:bg-[#eeeeec] dark:border-[#eeeeec] dark:text-[#1C1C1A] dark:hover:bg-white dark:hover:border-white hover:bg-black hover:border-black px-5 py-1.5 bg-[#1b1b18] rounded-sm border border-black text-white text-sm leading-normal">
+                            class="inline-block dark:bg-[#eeeeec] dark:border-[#eeeeec] dark:text-[#1C1C1A] dark:hover:bg-white dark:hover:border-white hover:bg-black hover:border-black px-5 py-1.5 bg-blue-600 rounded-sm border border-black text-white text-sm leading-normal">
                             Get Started with New Account
                         </a>
                     </li>
