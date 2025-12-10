0b593dcfe3b343c3c958b6ac223827238796a448
bxlr_ <nabil@runcloud.io>
2025-12-10 20:37:24 +0800

    feat: add ExpenseSeeder, use UserFactory in DatabaseSeeder

---
+++ database/seeders/ExpenseSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class ExpenseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('expenses')->insert([
+            'user_id' => 1,
+            'title' => 'Dummy Expense',
+            'description' => 'Dummy Description',
+            'amount' => 100,
+            'category' => 'Meal',
+            'notes' => null,
+        ]);
+    }
+}
