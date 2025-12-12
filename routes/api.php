66fe060c757810576d1a1d9cb4fabc6320f3c849
bxlr_ <nabil@runcloud.io>
2025-12-12 22:10:49 +0800

    feat: install api

---
+++ routes/api.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+Route::get('/users', function (Request $request) {
+    // return \App\Models\User::all();
+    return response()->json([
+        'users' => \App\Models\User::all()
+        // 'expenses' => 
+    ]);
+});
